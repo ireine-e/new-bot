c171a17c7afc6dc08ddd13550e3b328f8182d5ad
ireine-e <andie.djfelipe@gmail.com>
2024-11-02 22:37:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["redfields.com", ".gg/ireine.e"];
+const statusMessages = ["shop now !", "watching .gg/xerezaa", "xoxo, cely", "cely ♡ jay"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
