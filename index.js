c069760c25d72c3817f1bc63bdcb2142ed9c631c
ireine-e <andie.djfelipe@gmail.com>
2024-11-16 23:52:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["shop now !", "watching .gg/xerezaa", "xoxo, cely", "cely ♡ jay"];
+const statusMessages = ["clairo ♡", "drop orders!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
